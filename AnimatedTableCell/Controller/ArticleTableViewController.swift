1fe1acfa3d927ea3ffaacb3fde56ad12843ae56b
devxris <chuwei0915@gmail.com>
2017-09-25 11:21:57 +0800

    Animate cell with different effects by CATransform3D on cell.layer.transform

--- AnimatedTableCell/Controller/ArticleTableViewController.swift
+++ AnimatedTableCell/Controller/ArticleTableViewController.swift
@@ -38,4 +38,27 @@ class ArticleTableViewController: UITableViewController {
 		cell.postImageView.image = UIImage(named: postImages[indexPath.row])
 		return cell
 	}
+	
+	/* Animating the cell with fading alpha while scrolling
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		// set initial state
+		cell.alpha = 0
+		// animate to target state
+		UIView.animate(withDuration: 1.0) { cell.alpha = 1 }
+	} */
+	
+	/* Animating the cell with rotation by CATransform3DMakeRotation
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		let rotationAngleInRadians = 90.0 * CGFloat.pi / 180
+		let rotationTransform = CATransform3DMakeRotation(rotationAngleInRadians, 0, 0, 1)
+		cell.layer.transform = rotationTransform
+		UIView.animate(withDuration: 1.0) { cell.layer.transform = CATransform3DIdentity }
+	} */
+	
+	// Animating the cell with flyin by CATransform3DTranslate
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		let flyingTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 0, 0)
+		cell.layer.transform = flyingTransform
+		UIView.animate(withDuration: 1.0) { cell.layer.transform = CATransform3DIdentity }
+	}
 }
