0db8573f49e4b8524c1be14490be0e76e5e78f59
devxris <chuwei0915@gmail.com>
2017-09-25 10:35:07 +0800

    Simple UI, Model

---
+++ AnimatedTableCell/Controller/ArticleTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleTableViewController.swift
+//  AnimatedTableCell
+//
+//  Created by Chris Huang on 25/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewController: UITableViewController {
+
+	// MARK: Model
+	private let postTitles = ["Use Background Transfer Service To Download File in Background",
+	                  "Face Detection in iOS Using Core Image",
+	                  "Building a Speech-to-Text App Using Speech Framework in iOS 10",
+	                  "Building Your First Web App in Swift Using Vapor",
+	                  "Creating Gradient Colors Using CAGradientLayer",
+	                  "A Beginner's Guide to CALayer"]
+	private let postImages = ["imessage-sticker-pack", "face-detection-featured", "speech-kit-featured",
+	                          "vapor-web-framework", "cagradientlayer-demo", "calayer-featured"]
+
+
+}
---
+++ AnimatedTableCell/View/ArticleCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleCell.swift
+//  AnimatedTableCell
+//
+//  Created by Chris Huang on 25/09/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var postImageView: UIImageView!
+	
+}
